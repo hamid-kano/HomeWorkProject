cbfb69287b888385f9a0b1aa1319e08e974af66d
hk-opead <hko-1992@hotmail.com>
2025-08-13 13:46:08 +0300

    TEST

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'department',
+        'position',
+        'salary',
+        'hire_date',
+        'status'
+    ];
 }
